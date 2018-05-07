a86b322263d3666b2cc0e88ad8347ee7f509893c
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-07 18:57:19 +0700

    form

--- src/app/form-sign-up/form-sign-up.component.ts
+++ src/app/form-sign-up/form-sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-sign-up',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-sign-up.component.css']
 })
 export class FormSignUpComponent implements OnInit {
-
-  constructor() { }
+  formSignUp: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.formSignUp = this.fb.group({
+      email: ['vanpho@gmail.com'],
+      password: ['', Validators.required],
+      rePassword: ['', Validators.required],
+    }, { validator: passwordMustMatch });
   }
 
+  get shouldShowPasswordError() {
+    const passwordControl = this.formSignUp.get('password');
+    const rePasswordControl = this.formSignUp.get('rePassword');
+    return passwordControl.touched && rePasswordControl.dirty && this.formSignUp.errors && this.formSignUp.errors.passwordMustMatch;
+  }
+}
+
+function passwordMustMatch(formGroup: FormGroup) {
+  const passwordControl = formGroup.get('password');
+  const rePasswordControl = formGroup.get('rePassword');
+  if (passwordControl.value === rePasswordControl.value) return null;
+  return { passwordMustMatch: true };
 }
