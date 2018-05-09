182cb04612f15c6dfcba4a5ec5de921df8046cde
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-09 18:51:52 +0700

    chat

--- src/app/chat.component.ts
+++ src/app/chat.component.ts
@@ -14,17 +14,22 @@ import io from 'socket.io-client';
                 >
                 <button class="btn btn-success" (click)="send();">Send</button>
             </div>
+            <div *ngFor="let m of messages">
+                <div class="text-info">{{ m }}</div>
+            </div>
         </div>
     `
 })
 
 export class ChatComponent {
     socket: any;
+    message = '';
+    messages = [];
+
     constructor() {
         this.socket = io.connect('https://socket1901.herokuapp.com/');
-        this.socket.on('SERVER_SEND_MESSAGE', data => alert(data));
+        this.socket.on('SERVER_SEND_MESSAGE', data => this.messages.push(data));
     }
-    message = '';
 
     send() {
         this.socket.emit('CLIENT_SEND_MESSAGE', this.message);
